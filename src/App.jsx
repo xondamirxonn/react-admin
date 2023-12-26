eb4f7bc5918fb3deb3958c3f3b84ca2ee17d9bcd
Xondamir <abdulazizovxondamirxon@gmail.com>
2023-12-27 00:06:04 +0500

    ff

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Todolist from './Components/Todolist';
+import { Container } from "react-bootstrap";
+function App() {
+
+  return (
+    <Container >
+      <Todolist />
+    </Container>
+  )
+}
+
+export default App
---
+++ src/Components/Todolist.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import axios from "axios";
+
+const Todolist = () => {
+  const [users, setUsers] = useState([]);
+  console.log(users);
+  useEffect(() => {
+    axios.get("http://localhost:3000/users").then((res) => {
+      setUsers(res.data);
+    });
+  }, []);
+
+  return (
+    <Container>
+      <table className="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => {
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.age}</td>
+            </tr>;
+          })}
+        </tbody>
+      </table>
+    </Container>
+  );
+};
+
+export default Todolist;
